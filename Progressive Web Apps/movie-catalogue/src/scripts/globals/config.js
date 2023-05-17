c7e703f81280054f75e432a19da8bbc60d466b18
DANNN-GIT <e41201452@student.polije.ac.id>
2023-05-18 02:09:43 +0700

    Progress Belajar Sub-Modul PWA

---
+++ Progressive Web Apps/movie-catalogue/src/scripts/globals/config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+  KEY: '11d7efd6d712c860c435bc0b499279ca',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+};
+
+export default CONFIG;
